235ab90eab73758bd16e28aa938f8111482f66d6
kimgaeul <ge950120@gmail.com>
2024-11-22 17:59:39 +0900

    컴포넌트 작업 중

--- client/src/pages/ClassPass/HistoryList.js
+++ client/src/pages/ClassPass/HistoryList.js
@@ -1,24 +1,27 @@
 import React from "react";
 
 const HistoryList = ({items}) => {
+
     return (
         <>
-        {items.map((item, index) => {
-            console.log(item);
-            <div key={index} className="title">{item.date}</div>
-        })}
-
-        {/*<div className="title">2024년 11월 13일 (수)</div>
-        <ul className="list">
-            <li>
-            <div className="history-line"><span className="label-box gray">예약 취소</span> 09 : 00 ~ 09 : 50</div>
-            <div className="class-name">빙수 코치</div>
-            </li>
-            <li>
-            <div className="history-line"><span className="label-box gray">예약 취소</span> 09 : 00 ~ 09 : 50</div>
-            <div className="class-name">빙수 코치</div>
-            </li>
-        </ul>*/}
+        {items.map((item, index) => (
+            <div key={index}>
+                <div className="title">{item.date}</div>
+                <ul className="list">
+                    {item.lists.map((list, listIndex) => (
+                        <li key={listIndex}>
+                            <div className="history-line">
+                                {list.map((label, labelIdx) => (
+                                    <span key={labelIdx} className={label.color}></span>{label.text}
+                                ))}
+                                {list.classTime}
+                            </div>
+                            <div className="class-name">{list.className}</div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ))}
         </>
     )
 }
